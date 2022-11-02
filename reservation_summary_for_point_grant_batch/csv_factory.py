10fdeb2a731bf80206ab4fc45e8496b0d88a10c4
a-takao <a-takao@dyn.co.jp>
2022-11-02 17:27:12 +0900

    出力ファイル名の日付フォーマット調整

--- reservation_summary_for_point_grant_batch/csv_factory.py
+++ reservation_summary_for_point_grant_batch/csv_factory.py
@@ -4,7 +4,7 @@ import dyconfig
 output_csv_path = dyconfig.get('output_csv', 'output_path')
 
 def generate_summary_csv_file(reserve_list, fromdate, todate, member_group_code):
-    output_csv_name = f'{output_csv_path}/aggregated_{fromdate}_{todate}_{member_group_code}.csv'
+    output_csv_name = f'{output_csv_path}/aggregated_{fromdate::%Y%m%d}_{todate:%Y%m%d}_{member_group_code}.csv'
     with open(output_csv_name, 'w', newline='') as csvfile:
         sqlwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         sqlwriter.writerow(['MEMBER_CODE', 'PLAN_CODE', 'RESERVE_NUMBER', 'ACTUAL_PRICE', 'TOTAL_USE_POINT_AMOUNT'])
