664df9db0680051dd69eb07a268db5c4c4986bfd
a-takao <a-takao@dyn.co.jp>
2022-11-02 12:29:44 +0900

    ポイント利用量とオプション金額取得のためのクエリ改修

--- reservation_summary_for_point_grant_batch/reserve_repository.py
+++ reservation_summary_for_point_grant_batch/reserve_repository.py
@@ -10,10 +10,25 @@ def get_reserve_summary(connection, member_group_codes, fromdate, todate):
     query = f'''
         SELECT MEMBER_CODE
             , PLAN_CODE
-            , RESERVE_NUMBER
-            , LODGING_TOTAL_PRICE
-        FROM reserveServiceDB.RESERVE_INFO_MASTER 
+            , reserve.RESERVE_NUMBER
+            , LODGING_TOTAL_PRICE + IFNULL(TOTAL_OPTION_PRICE, 0) + IFNULL(ACCOMMODATION_RECORD_ADJUSTMENT_PRICE, 0) AS `ACTUAL_AMOUNT`
+            , IFNULL(TOTAL_USE_POINT_AMOUNT, 0) AS `TOTAL_USE_POINT_AMOUNT`
+        FROM reserveServiceDB.RESERVE_INFO_MASTER reserve
+        LEFT JOIN (
+            SELECT RESERVE_NUMBER, SUM(AMOUNT) AS `TOTAL_USE_POINT_AMOUNT`
+            FROM reserveServiceDB.USED_POINT
+            WHERE KIND = 'PAYMENT'
+            GROUP BY RESERVE_NUMBER
+        ) point
+            ON point.RESERVE_NUMBER
+        LEFT JOIN (
+            SELECT RESERVE_NUMBER, SUM(OPTION_RESERVE_NUM * OPTION_PRICE) AS `TOTAL_OPTION_PRICE`
+            FROM reserveServiceDB.RESERVE_OPTION_TABLE
+            GROUP BY RESERVE_NUMBER
+        ) reserve_option
+            ON reserve_option.RESERVE_NUMBER = reserve.RESERVE_NUMBER
         WHERE MEMBER_TYPE_KBN = 1
+        AND RESERVE_CANCEL_KBN = 0
         AND '{fromdate}' <= DATE_ADD(RESERVE_CHECKIN_DATE, INTERVAL RESERVE_LODGING_DATE_NUM + {grace_days_after_checkout} DAY)
         AND '{todate}' > DATE_ADD(RESERVE_CHECKIN_DATE, INTERVAL RESERVE_LODGING_DATE_NUM + {grace_days_after_checkout} DAY)
         AND MEMBER_GROUP_CODE IN ({member_group_code_term});
