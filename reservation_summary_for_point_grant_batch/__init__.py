9e65c3dd2ace2b608ba27ed2f0cf0f2dbccad691
a-takao <a-takao@dyn.co.jp>
2022-11-08 14:07:50 +0900

    ひとまずエラーキャッチして内容をログに吐かせる

--- reservation_summary_for_point_grant_batch/__init__.py
+++ reservation_summary_for_point_grant_batch/__init__.py
@@ -12,40 +12,47 @@ from datetime import date
 logger = setup.get_logger()
 
 
-class ExitCode(enum.IntEnum):
+class ResultCode(enum.IntEnum):
     SUCCESS = 0
-    ERROR = 1
+    OTHER_ERROR = 1
 
 
-def main() -> int:
+def main() -> ResultCode:
     """
     fromdateからtodateまでの間に実績確定した予約の一覧を抽出する。
     """
-    logger.info("reservation_summary_for_point_grant_batch Start...")
 
-    fromdate = date.fromisoformat(sys.argv[1])
-    todate = date.fromisoformat(sys.argv[2])
+    try:
+        logger.info("reservation_summary_for_point_grant_batch Start...")
 
-    member_group_codes = member_repository.get_member_group_codes()
-    reserve_service_connection = dbconnecter.get_connection("reserveServiceDB")
+        fromdate = date.fromisoformat(sys.argv[1])
+        todate = date.fromisoformat(sys.argv[2])
 
-    with reserve_service_connection:
-        reserve_map_by_group = reserve_repository.get_reserve_summary(
-            connection=reserve_service_connection,
-            member_group_codes=member_group_codes,
-            fromdate=fromdate,
-            todate=todate)
+        member_group_codes = member_repository.get_member_group_codes()
+        reserve_service_connection = dbconnecter.get_connection(
+            "reserveServiceDB")
 
-    for member_group_code, reserve_list in reserve_map_by_group:
-        csv_factory.generate_summary_csv_file(
-            reserve_list=reserve_list,
-            fromdate=fromdate,
-            todate=todate,
-            member_group_code=member_group_code)
+        with reserve_service_connection:
+            reserve_map_by_group = reserve_repository.get_reserve_summary(
+                connection=reserve_service_connection,
+                member_group_codes=member_group_codes,
+                fromdate=fromdate,
+                todate=todate)
 
-    logger.info("reservation_summary_for_point_grant_batch End")
+        for member_group_code, reserve_list in reserve_map_by_group:
+            csv_factory.generate_summary_csv_file(
+                reserve_list=reserve_list,
+                fromdate=fromdate,
+                todate=todate,
+                member_group_code=member_group_code)
 
-    return ExitCode.SUCCESS
+        logger.info("reservation_summary_for_point_grant_batch End")
+
+        return ResultCode.SUCCESS
+
+    except Exception as exception:
+        logger.error(exception)
+        return ResultCode.OTHER_ERROR
 
 
 if __name__ == "__main__":
