ba6d2a762d6802f8de94f2970ff088a418379512
a-takao <a-takao@dyn.co.jp>
2022-11-10 17:20:00 +0900

    dbコネクション取得メソッドのアサーション内容を調整

--- reservation_summary_for_point_grant_batch/tests/test_dbconnecter.py
+++ reservation_summary_for_point_grant_batch/tests/test_dbconnecter.py
@@ -6,37 +6,47 @@ from reservation_summary_for_point_grant_batch import dbconnecter, dyconfig
 CONFIG_DATA = {
     "name1": {
         "host": "192.168.11.21",
+        "port": 3306,
         "user": "root",
         "password": "root",
         "database": "reserveServiceDB",
+        "ssl_disabled": True,
     },
     "name2": {
         "host": "192.168.1.2",
+        "port": 12000,
         "user": "testuser2",
         "password": "testpass2",
         "database": "testDB2",
+        "ssl_disabled": False,
     },
 }
 
 
 class Test_dbconnecter(unittest.TestCase):
 
-    @mock.patch.object(dyconfig, "getboolean", return_value=True)
-    @mock.patch.object(dyconfig, "getint", return_value=3306)
+    @mock.patch.object(mysql.connector, "connect")
+    @mock.patch.object(dyconfig, "getboolean")
+    @mock.patch.object(dyconfig, "getint")
     @mock.patch.object(dyconfig, "get")
-    def test_get_connection(self, mocked_get, mocked_getint, mocked_getboolean):
-        mocked_get.side_effect = lambda name, key: CONFIG_DATA[name][key]
+    def test_get_connection(self, mocked_get, mocked_getint, mocked_getboolean, mocked_connect):
         expected_name = "name1"
         expected_host = CONFIG_DATA[expected_name]["host"]
-        expected_port = mocked_getint.return_value
+        expected_port = CONFIG_DATA[expected_name]["port"]
         expected_user = CONFIG_DATA[expected_name]["user"]
         expected_password = CONFIG_DATA[expected_name]["password"]
         expected_database = CONFIG_DATA[expected_name]["database"]
-        expected_ssl_disabled = mocked_getboolean.return_value
+        expected_ssl_disabled = CONFIG_DATA[expected_name]["ssl_disabled"]
 
-        actual = dbconnecter.get_connection(expected_name)
+        mocked_get.side_effect = lambda name, key: CONFIG_DATA[name][key]
+        mocked_getint.return_value = expected_port
+        mocked_getboolean.return_value = expected_ssl_disabled
 
-        self.assertEqual(actual._host, expected_host)
+        actual_connection = dbconnecter.get_connection(expected_name)
+
+        mocked_connect.assert_called_once_with(
+            host=expected_host, port=expected_port, user=expected_user, password=expected_password, database=expected_database, ssl_disabled=expected_ssl_disabled)
+        self.assertFalse(actual_connection.autocommit)
 
 
 if __name__ == '__main__':
