012c05be941c89486ad61d1ca57491d70c2cc0aa
a-takao <a-takao@dyn.co.jp>
2022-11-11 21:24:45 +0900

    異常終了パターンも簡単に追加

--- reservation_summary_for_point_grant_batch/tests/test___init__.py
+++ reservation_summary_for_point_grant_batch/tests/test___init__.py
@@ -18,7 +18,7 @@ class test___init__(unittest.TestCase):
 
     @mock.patch.object(dbconnecter, "get_connection")
     @mock.patch.object(dyconfig, "get")
-    def test_main(self,  mocked_get, mocked_connection):
+    def test_main_正常終了(self,  mocked_get, mocked_connection):
         mocked_get.side_effect = lambda name, key: self.CONFIG_DATA[name][key]
 
         arg_fromdate = "2021-09-05"
@@ -111,6 +111,22 @@ class test___init__(unittest.TestCase):
                 actual_csvfile, delimiter=",", quotechar='"')
             actual_csv_data_list_2 = [row for row in spamreader]
         assert actual_csv_data_list_2 == expected_file_content_2
+
+    @mock.patch.object(dbconnecter, "get_connection")
+    @mock.patch.object(dyconfig, "get")
+    def test_main_異常終了(self,  mocked_get, mocked_connection):
+        mocked_get.side_effect = Exception("test_exception")
+
+        arg_fromdate = "2021-09-05"
+        arg_todate = "2022-09-06"
+
+        del sys.argv[1:]
+        sys.argv.append(arg_fromdate)
+        sys.argv.append(arg_todate)
+        actual_result = main()
+        del sys.argv[1:]
+
+        assert actual_result == ResultCode.OTHER_ERROR
 
 
 if __name__ == '__main__':
