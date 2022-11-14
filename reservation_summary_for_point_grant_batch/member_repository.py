4e289fc58cba8d0b3833884341df147298811b01
a-takao <a-takao@dyn.co.jp>
2022-11-14 10:04:10 +0900

    http通信の仮実装を外す

--- reservation_summary_for_point_grant_batch/member_repository.py
+++ reservation_summary_for_point_grant_batch/member_repository.py
@@ -10,13 +10,13 @@ action = dyconfig.get("member_repository", "member_group_code_api_path")
 
 
 def get_member_group_codes() -> list[str]:
-    return ["hoge", "huga", "M000000060", "M000000019"]
     url = f"{base_path}/{action}"
 
     parameters = {"hoge": "hoge", "huga": 10}
 
     response = requests.get(url, parameters)
     response.raise_for_status()
-    result_member_group_codes = json.loads(response.text)
+    logger.debug(response.text)
+    result_member_group_codes = response.json()
 
     return result_member_group_codes
