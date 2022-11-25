4505170155a0c46d13ccb8558bb4d06339e73503
a-takao <a-takao@dyn.co.jp>
2022-11-25 19:24:31 +0900

    モックデータとアサーション用のデータを外置き出来るような仕組みの構築

--- reservation_summary_for_point_grant_batch/tests/test___init__.py
+++ reservation_summary_for_point_grant_batch/tests/test___init__.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
-import csv
-from decimal import Decimal
+import json
+import glob
 from reservation_summary_for_point_grant_batch import ResultCode, main, setup
 
 
@@ -13,82 +13,49 @@ CONFIG_DATA = {
 }
 
 
-def test_main_正常終了(mocked_config_get, mocked_http_get, mocked_connection):
+def init_testdata_list():
+    testdata_path = "reservation_summary_for_point_grant_batch/tests/testdata/case*/"
+    case_list = glob.glob(testdata_path)
+
+    testdata_list = []
+    for case_dir in case_list:
+        with open(f"{case_dir}/query_result.json") as json_file:
+            query_result = json.load(json_file)
+
+        csv_file_list = glob.glob(f"{case_dir}/expected_summary_*.csv")
+        expected_csv_list = []
+        for csv_file_name in csv_file_list:
+            with open(csv_file_name, newline="\r\n") as csv_file:
+                expected_csv_list.append(csv_file.read())
+        testdata_list.append((query_result, expected_csv_list))
+
+    return testdata_list
+
+
+@pytest.fixture(params=init_testdata_list())
+def testdata(request):
+    return request.param
+
+
+def test_main_正常終了(mocked_config_get, mocked_http_get, mocked_connection, testdata):
     mocked_config_get.side_effect = lambda name, key: CONFIG_DATA[name][key]
+    (query_result, expected_csv_list) = testdata
 
     arg_fromdate = "2021-09-05"
     arg_todate = "2022-09-06"
-    arg_group_code_1 = "M000000060"
-    arg_group_code_2 = "M000000019"
+    arg_group_code_list = sorted(
+        set([row["MEMBER_GROUP_CODE"] for row in query_result]))
 
-    api_response_body = ["hoge", "huga", arg_group_code_1, arg_group_code_2]
+    api_response_body = ["hoge", "huga"] + arg_group_code_list
     mocked_http_get.return_value.json.return_value = api_response_body
 
-    query_result = [
-        {
-            "MEMBER_GROUP_CODE": arg_group_code_1,
-            "MEMBER_CODE": "member1",
-            "PLAN_CODE": "plan1",
-            "RESERVE_NUMBER": "reserve1",
-            "ACTUAL_PRICE": Decimal("6200"),
-            "TOTAL_USE_POINT_AMOUNT": Decimal("1000"),
-        },
-        {
-            "MEMBER_GROUP_CODE": arg_group_code_1,
-            "MEMBER_CODE": "member2",
-            "PLAN_CODE": "plan2",
-            "RESERVE_NUMBER": "reserve2",
-            "ACTUAL_PRICE": Decimal("12000"),
-            "TOTAL_USE_POINT_AMOUNT": Decimal("0"),
-        },
-        {
-            "MEMBER_GROUP_CODE": arg_group_code_2,
-            "MEMBER_CODE": "member3",
-            "PLAN_CODE": "plan3",
-            "RESERVE_NUMBER": "reserve3",
-            "ACTUAL_PRICE": Decimal("1500"),
-            "TOTAL_USE_POINT_AMOUNT": Decimal("500"),
-        }, ]
     mocked_cursor = mocked_connection.return_value.cursor.return_value
     mocked_cursor.fetchmany.side_effect = [query_result, []]
 
     param_fromdate = arg_fromdate.replace("-", "")
     param_todate = arg_todate.replace("-", "")
-    expected_file_name_1 = f"output/summary_reserve_{param_fromdate}_{param_todate}_{arg_group_code_1}.csv"
-    expected_file_name_2 = f"output/summary_reserve_{param_fromdate}_{param_todate}_{arg_group_code_2}.csv"
-    expected_fieldnames = [
-        "MEMBER_CODE",
-        "PLAN_CODE",
-        "RESERVE_NUMBER",
-        "ACTUAL_PRICE",
-        "TOTAL_USE_POINT_AMOUNT"]
-    expected_file_content_1 = [
-        expected_fieldnames,
-        [
-            query_result[0]["MEMBER_CODE"],
-            query_result[0]["PLAN_CODE"],
-            query_result[0]["RESERVE_NUMBER"],
-            str(query_result[0]["ACTUAL_PRICE"]),
-            str(query_result[0]["TOTAL_USE_POINT_AMOUNT"]),
-        ],
-        [
-            query_result[1]["MEMBER_CODE"],
-            query_result[1]["PLAN_CODE"],
-            query_result[1]["RESERVE_NUMBER"],
-            str(query_result[1]["ACTUAL_PRICE"]),
-            str(query_result[1]["TOTAL_USE_POINT_AMOUNT"]),
-        ],
-    ]
-    expected_file_content_2 = [
-        expected_fieldnames,
-        [
-            query_result[2]["MEMBER_CODE"],
-            query_result[2]["PLAN_CODE"],
-            query_result[2]["RESERVE_NUMBER"],
-            str(query_result[2]["ACTUAL_PRICE"]),
-            str(query_result[2]["TOTAL_USE_POINT_AMOUNT"]),
-        ],
-    ]
+    expected_file_name_list = [
+        f"output/summary_reserve_{param_fromdate}_{param_todate}_{group_code}.csv"for group_code in arg_group_code_list]
 
     del sys.argv[1:]
     sys.argv.append(arg_fromdate)
@@ -98,17 +65,11 @@ def test_main_正常終了(mocked_config_get, mocked_http_get, mocked_connection
 
     assert actual_result == ResultCode.SUCCESS
 
-    with open(expected_file_name_1, newline='') as actual_csvfile:
-        spamreader = csv.reader(
-            actual_csvfile, delimiter=",", quotechar='"')
-        actual_csv_data_list_1 = [row for row in spamreader]
-    assert actual_csv_data_list_1 == expected_file_content_1
-
-    with open(expected_file_name_2, newline='') as actual_csvfile:
-        spamreader = csv.reader(
-            actual_csvfile, delimiter=",", quotechar='"')
-        actual_csv_data_list_2 = [row for row in spamreader]
-    assert actual_csv_data_list_2 == expected_file_content_2
+    actual_csv_list = []
+    for expected_file_name in expected_file_name_list:
+        with open(expected_file_name, newline="\r\n") as actual_csvfile:
+            actual_csv_list.append(actual_csvfile.read())
+    assert actual_csv_list == expected_csv_list
 
 
 def test_main_異常終了(mocked_config_get, mocked_http_get, mocked_connection):
