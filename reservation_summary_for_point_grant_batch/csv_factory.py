94fafcbe4a238abb67de6518334a2c78dc8dcea0
a-takao <a-takao@dyn.co.jp>
2022-11-28 18:32:19 +0900

    fetchしてきた予約情報をリスト化せず個別ループで処理する方針でアルゴリズム修正

--- reservation_summary_for_point_grant_batch/csv_factory.py
+++ reservation_summary_for_point_grant_batch/csv_factory.py
@@ -9,49 +9,52 @@ from . import csv_factory
 logger = setup.get_logger(__name__)
 output_csv_path = dyconfig.get("output_csv", "output_path")
 
-KEY_NAME = "MEMBER_GROUP_CODE"
-
 
 def convert_reserve_to_csv(reserve_list_cursor, fromdate: date, todate: date):
     reserve_list: list[dict[str, str]] = []
+    latest_group_code: str = ""
+
     while True:
         latest_reserve_list = reserve_repository.get_reserve_list(
             reserve_list_cursor)
-        reserve_list += latest_reserve_list
 
-        latest_group_code = ""
-        if latest_reserve_list:
-            latest_group_code = latest_reserve_list[-1][KEY_NAME]
+        if not latest_reserve_list:
+            break
 
-        loaded_reserve_list = [
-            reserve for reserve in reserve_list if reserve[KEY_NAME] != latest_group_code]
-        reserve_list = [
-            reserve for reserve in reserve_list if reserve[KEY_NAME] == latest_group_code]
+        for target_reserve in latest_reserve_list:
+            target_group_code = target_reserve["MEMBER_GROUP_CODE"]
 
-        target_summary = itertools.groupby(
-            loaded_reserve_list, lambda reserve: reserve.pop(KEY_NAME))
+            if latest_group_code and target_group_code != latest_group_code:
+                csv_factory.generate_summary_csv_file(
+                    reserve_list=reserve_list,
+                    fromdate=fromdate,
+                    todate=todate,
+                    member_group_code=latest_group_code)
+                reserve_list = []
 
-        for member_group_code, target_reserve_list in target_summary:
-            csv_factory.generate_summary_csv_file(
-                reserve_list=list(target_reserve_list),
-                fromdate=fromdate,
-                todate=todate,
-                member_group_code=member_group_code)
+            reserve_list.append(target_reserve)
+            latest_group_code = target_group_code
 
-        if not reserve_list:
-            return
+    if reserve_list:
+        csv_factory.generate_summary_csv_file(
+            reserve_list=reserve_list,
+            fromdate=fromdate,
+            todate=todate,
+            member_group_code=latest_group_code)
 
 
 def generate_summary_csv_file(reserve_list: list[dict[str, str]], fromdate: date, todate: date, member_group_code: str) -> None:
     output_csv_name = f"{output_csv_path}/summary_reserve_{fromdate:%Y%m%d}_{todate:%Y%m%d}_{member_group_code}.csv"
     logger.info("%s, size: %s", output_csv_name, len(reserve_list))
+    fieldnames = [
+        "MEMBER_CODE",
+        "PLAN_CODE",
+        "RESERVE_NUMBER",
+        "ACTUAL_PRICE",
+        "TOTAL_USE_POINT_AMOUNT"]
+    csv_data_list = [{key: value for key, value in reserve.items(
+    ) if key in fieldnames} for reserve in reserve_list]
     with open(output_csv_name, "w", newline="") as csvfile:
-        fieldnames = [
-            "MEMBER_CODE",
-            "PLAN_CODE",
-            "RESERVE_NUMBER",
-            "ACTUAL_PRICE",
-            "TOTAL_USE_POINT_AMOUNT"]
         csvwriter = csv.DictWriter(
             csvfile,
             fieldnames=fieldnames,
@@ -59,4 +62,4 @@ def generate_summary_csv_file(reserve_list: list[dict[str, str]], fromdate: date
             quotechar='"',
             quoting=csv.QUOTE_ALL)
         csvwriter.writeheader()
-        csvwriter.writerows(reserve_list)
+        csvwriter.writerows(csv_data_list)
