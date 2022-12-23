5d16fd04c9a0e94049d0363fe96a778775982d26
a-takao <a-takao@dyn.co.jp>
2022-12-23 20:57:50 +0900

    ホテルグループコード一覧取得の中身を正式に実装

--- reservation_summary_batch/hotel_group_repository.py
+++ reservation_summary_batch/hotel_group_repository.py
@@ -3,18 +3,60 @@ from . import dyconfig
 from . import setup
 
 logger = setup.get_logger(__name__)
-base_path = dyconfig.get("member_repository", "crm_api_root")
-action = dyconfig.get("member_repository", "member_group_code_api_path")
+realm = dyconfig.get("hotel_group_repository", "realm")
+base_url = dyconfig.get("hotel_group_repository", "cdp_api_root")
+list_size = dyconfig.getint("hotel_group_repository", "list_size")
+client_id = dyconfig.get("hotel_group_repository", "client_id")
+client_secret = dyconfig.get("hotel_group_repository", "client_secret")
 
 
 def get_member_group_codes() -> list[str]:
-    url = f"{base_path}/{action}"
+    access_token = _get_access_token()
+    header = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 
-    parameters = {"hoge": "hoge", "huga": 10}
+    url = f"{base_url}/admin/realms/{realm}/cdp/realm/list"
+    result_member_group_code_list: list[str] = []
+    page = 1
 
-    response = requests.get(url, parameters)
+    while True:
+        logger.debug("page: %s, size: %s", page, list_size)
+
+        parameters = {"page": page, "size": list_size}
+
+        response = requests.get(url, data=parameters,
+                                headers=header, verify=False)
+        response.raise_for_status()
+        logger.debug(response.text)
+        member_group_list = response.json()
+
+        logger.debug("result_size: %s", len(member_group_list))
+
+        if not member_group_list:
+            break
+
+        member_group_code_list = [member["id"] for member in member_group_list]
+        result_member_group_code_list.extend(member_group_code_list)
+        page += 1
+
+    return result_member_group_code_list
+
+
+def _get_access_token() -> str:
+    url = f"{base_url}/realms/{realm}/protocol/openid-connect/token"
+    parameters = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
+    header = {"Content-Type": "application/x-www-form-urlencoded"}
+
+    response = requests.post(url, data=parameters,
+                             headers=header, verify=False)
     response.raise_for_status()
-    logger.debug(response.text)
-    result_member_group_codes = response.json()
+    response_json = response.json()
+    access_token = response_json["access_token"]
 
-    return result_member_group_codes
+    return access_token
